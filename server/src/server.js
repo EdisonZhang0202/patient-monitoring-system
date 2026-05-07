a5c05b0e85d1059b7d949623e2fb22a84766f5a9
Edison Zhang <edisonzhang714@gmail.com>
2026-05-07 17:28:00 -0400

    Add MongoDB connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,15 @@
+import dotenv from "dotenv";
 import http from "http";
 import app from "./app.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
+
+connectDB();
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
